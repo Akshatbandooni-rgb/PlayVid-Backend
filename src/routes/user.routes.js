3e6de7fc794cdc7f774e054a6de0d2f08a9032e8
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-01 00:00:29 +0530

    Fixed Typo

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const userRouter = express.Router();
 const { registerUser } = require("../controllers/user.controller");
 
-router.get("/register", registerUser);
+userRouter.get("/register", registerUser);
 
 module.exports = userRouter;
